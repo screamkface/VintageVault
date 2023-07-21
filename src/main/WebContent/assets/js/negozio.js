b3cb25901b970ee26044136578e82dccd56739d2
screamkface <79250777+screamkface@users.noreply.github.com>
2023-07-21 20:20:30 +0200

    upload cartella webcontent aggiornata

---
+++ src/main/WebContent/assets/js/negozio.js
@@ -0,0 +1,13 @@
+function toggleFilterMenu() {
+  document.querySelector(".filtri").style.display = "block";
+}
+document
+  .querySelectorAll(".show-filtri")
+  .forEach((el) => el.addEventListener("click", toggleFilterMenu));
+  
+  
+  
+  
+  
+  
+  
